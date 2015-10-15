e8b9b2bc3e2a45b378176cd51d3549dadc2d329a
joe62 <yunfei_z@163.com>
2015-10-15 09:41:20 +0800

    增加第13章源代码

---
+++ ASP.NET Identity2.0学习笔记/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Users.Models;
+
+namespace Users.Infrastructure
+{
+    public class AppIdentityDbContext: IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext():base("IdentityDb"){}
+        static  AppIdentityDbContext()
+        {
+            Database.SetInitializer<AppIdentityDbContext>(new IdentityDbInit());
+        }
+        public static AppIdentityDbContext Create()
+        {
+            return new AppIdentityDbContext();
+        }
+        public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+        {
+            protected override void Seed(AppIdentityDbContext context)
+            {
+                PerformInitialSetup(context);
+                base.Seed(context);
+            }
+
+            public void PerformInitialSetup(AppIdentityDbContext context)
+            {
+                // 在此添加初始化配置
+            }
+        }
+    }
+}
